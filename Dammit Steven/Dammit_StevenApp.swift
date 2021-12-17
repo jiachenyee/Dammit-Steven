9fa9fc6989d6b0950dfdf04c7603902d0f933bfe
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-12-17 19:14:20 +0800

    Initial Commit

---
+++ Dammit Steven/Dammit_StevenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dammit_StevenApp.swift
+//  Dammit Steven
+//
+//  Created by Jia Chen Yee on 17/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Dammit_StevenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
